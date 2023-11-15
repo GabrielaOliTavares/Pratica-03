8d72acd5dc2905008b0698f637f5b3bcc0c1f1c1
GabrielaOliTavares <145030166+GabrielaOliTavares@users.noreply.github.com>
2023-11-15 18:40:12 -0300

    Update pessoa.php

--- pessoa.php
+++ pessoa.php
@@ -2,10 +2,12 @@
 // privados e os valores devem ser informados via construtor.
 
 <?php
+// As classes em PHP são usadas para definir objetos e encapsular propriedades e métodos relacionados.
 class Pessoa {
 private $nome = "";
-private $idade = 0;
+private $idade = 0; //private indica que essas propriedades só podem ser acessadas dentro da própria classe e não fora dela.
 private $sexo = "";
+ // O construtor é chamado automaticamente quando um objeto é criado a partir da classe.
 public function __construct($nome, $idade, $sexo) {
 $this->nome = $nome;
 // $this faz referência à instância da classe atual. Ele é usado para acessar membros (propriedades e métodos)
@@ -16,7 +18,10 @@ $this->sexo = $sexo;
 
 // Na mesma classe, crie um método imprimir que retorne uma string no seguinte formato:
  //   Meu nome é <nome>, tenho <idade> anos e sou do sexo <sexo>.
-public function imprimir() {
+
+ // Métodos públicos podem ser acessados fora da classe.
+ 
+ public function imprimir() {
 return "Meu nome é " .
 $this->nome .
 ", tenho " .
@@ -24,6 +29,7 @@ $this->idade .
 " anos e sou do sexo " .
 $this->sexo .
 ".";
+  //  concatenando os valores das propriedades com texto adicional para formar a frase desejada.
 }
 }
 ?>
